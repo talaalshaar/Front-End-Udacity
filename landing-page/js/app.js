74179aae93209bb3a6deda5e952f0d839735dab9
tala <tala@Talas-MacBook-Air.local>
2022-02-02 00:18:52 +0200

    Project Rebuilt

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,106 +1,55 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  * 
 */
-const sections = Array.from(document.querySelectorAll('section'));
-const menu = document.getElementById('navbar__list');
-let numberOfListItems = sections.length;
+const Menu = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function createListItem() {
-    for (section of sections) {
-        sectionName = section.getAttribute('data-nav');
-        sectionLink = section.getAttribute('id');
 
-        //create item from each one
-        listItem = document.createElement('li');
 
-        //add items text
-        listItem.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${sectionName}</a>`;
-
-        //add listitem to menu
-        menu.appendChild(listItem);
-    }
-}
-
-//determine if section in viewport
-function sectionInViewPort(elem) {
-    let sectionPos = elem.getBoundingClientRect();
-    return (sectionPos.top >= 0);
-}
-
-//gives section beung viewed a different appearance
-function toggleActiveClass() {
-    for (section of sections) {
-        if (sectionInViewPort(section)) {
-            //if it doesnt contains you active class
-            if (!section.classList.contains('your-active-class')) {
-                //add it
-                section.classList.add('your-active-class');
-            }
-            else { // if it oustide viewport remove it
-                section.classList.remove('your-active-class');
-            }
-        }
-    }
-}
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
-var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+// build the nav
+NavBuilder = () => {
+    //to loop from section 1 to 4 and add sections to navigation 
+    for (s = 0; s <= sections.length; s++) {
+        //const el = document.querySelector('data-nav');
+        const liElem = document.createElement('li');
+        const Id = sections[s].id;
+        const DataNav = sections[s].dataset.nav;
+        liElem.innerHTML = `<li><a class="menu__link" href="#${Id}">${DataNav}</a></li>`;
+        Menu.appendChild(liElem);
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+window.addEventListener('scroll', (e) => {
+    sections.forEach(s => {
+        const topDistance = s.getBoundingClientRect().top;
+        // if the distance to the top is between 0-150px
+        if (topDistance >= 0 && topDistance < 150) {
+            s.classList.add('your-active-class');
+            // otherwise, remove the class
+        } else {
+            s.classList.remove('your-active-class');
+        }
+    });
 
-// build the nav
 
-createListItem();
+});
+
+NavBuilder();
 
 // Add class 'active' to section when near top of viewport
-document.addEventListener('scroll', toggleActiveClass);
-
-// Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
@@ -113,3 +62,5 @@ document.addEventListener('scroll', toggleActiveClass);
 // Scroll to section on link click
 
 // Set sections as active
+
+
