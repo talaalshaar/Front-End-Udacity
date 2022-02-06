5707cefacbef1971d42ee95c190aa8af0a3781ad
tala <tala@Talas-MacBook-Air.local>
2022-02-06 13:59:02 +0200

    highlight navbar when scrolling

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -11,29 +11,36 @@ const sections = document.querySelectorAll('section');
 NavBuilder = () => {
     //to loop from section 1 to 4 and add sections to navigation 
     for (s = 0; s < sections.length; s++) {
-        //const el = document.querySelector('data-nav');
         const liElem = document.createElement('li');
+        //const divElem = document.createElement("div");
         const Id = sections[s].id;
         const DataNav = sections[s].dataset.nav;
-        liElem.innerHTML = `<li><a class="menu__link" href="#${Id}">${DataNav}</a></li>`;
+        liElem.innerHTML = `<a class="menu__link" href="#${Id}">${DataNav}</a>`;
         Menu.appendChild(liElem);
-
     }
 }
 NavBuilder();
 
+
+//when scroll add your-active class to section and class active to navigation bar elements
 window.addEventListener('scroll', () => {
     sections.forEach(s => {
         const topDistance = s.getBoundingClientRect().top;
         // if the distance to the top is between 0-150px
         if (topDistance >= 0 && topDistance < 150) {
             s.classList.add('your-active-class');
+            // add class active to navugation in order to highlight the navigation when scrolling
+            document.querySelector(`a[href="#${s.id}"]`).classList.add("active");
+
             // otherwise, remove the class
         } else {
             s.classList.remove('your-active-class');
+            document.querySelector(`a[href="#${s.id}"]`).classList.remove("active");
+
 
         }
     });
+
 });
 
 //set style to highlight active navigation bar elements
@@ -50,19 +57,6 @@ addStyle = () => {
 }
 addStyle();
 
-// scrolling = () => {
-//     menuLink.forEach((anchor) => {
-//         menuLink.forEach((anchor) => {
-//             anchor.classList.remove("active");
-//         });
-//         anchor.classList.add("active");
-//     });
-// }
-
-
-// window.onscroll = function () {
-//     scrolling();
-// };
 
 
 scrollFunction = () => {
