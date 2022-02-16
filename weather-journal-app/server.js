642786603a1d242b9d58dd1d7c86983264e01eab
tala <tala@Talas-MacBook-Air.local>
2022-02-16 14:27:57 +0200

    creating server.js and installing node

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+//VlTrUlwJd7ZYy8etJPwkw8Mqa1jZCMzaEJmy7bV6
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
+const port = 8000;
 
 
 // Setup Server
+const server = app.listen(port, listening);
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);    
+}
+
